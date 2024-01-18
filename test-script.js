940eb71c15ab0c519260d5b0b19b091d99e60cec
Moka YT <141146299+MokaYT@users.noreply.github.com>
2024-01-18 22:05:25 +0300

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,4 +1,10 @@
- function setupUI(){
+window.loggedIn = false;
+const isMobile = () => (window.innerWidth < 800) && ("ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+window.onMobile = isMobile();
+
+console.log(`I am on ${isMobile() ? 'Mobile' : 'PC'}`);
+
+function setupUI(){
 		var style = document.createElement('style');
     style.type = 'text/css';
 
